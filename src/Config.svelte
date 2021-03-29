90095726d26cc2c9cc00029bb678197e52737e1a
Chris Putnam <chris@webfx.com>
2021-03-29 07:27:37 -0400

    GDrive config sync in progress

--- src/Config.svelte
+++ src/Config.svelte
@@ -1,9 +1,14 @@
 <script>
+  let isSignedIn = false;
+  let config = {
+    test: 'test123'
+  };
+  let error = '';
+
   /**
    *  On load, called to load the auth2 library and API client library.
    */
   function handleClientLoad() {
-    console.log('handleClientLoad');
     gapi.load('client:auth2', initClient);
   }
 
@@ -26,21 +31,73 @@
 
           // Handle the initial sign-in state.
           updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-          authorizeButton.onclick = handleAuthClick;
-          signoutButton.onclick = handleSignoutClick;
         },
         function (error) {
-          appendPre(JSON.stringify(error, null, 2));
+          error = JSON.stringify(error, null, 2);
         }
       );
+  }
+
+  /**
+   *  Called when the signed in status changes, to update the UI
+   *  appropriately. After a sign-in, the API is called.
+   */
+  function updateSigninStatus(_isSignedIn) {
+    isSignedIn = _isSignedIn;
+
+    // Try loading application config
+    if (isSignedIn) {
+      console.log('Getting config files');
+      gapi.client.drive.files
+        .list({
+          spaces: 'appDataFolder',
+          q: 'name = "config.json"',
+          fields: 'nextPageToken, files(id, name)',
+          pageSize: 100
+        })
+        .then(
+          function (response) {
+            if (response.result.files && response.result.files.length > 0) {
+              console.log(response.result.files.length);
+            } else {
+              error = 'No config file saved yet';
+            }
+          },
+          function (_error) {
+            error = JSON.stringify(_error);
+          }
+        );
+    }
+  }
+
+  /**
+   *  Sign in the user upon button click.
+   */
+  function signIn(event) {
+    gapi.auth2.getAuthInstance().signIn();
+  }
+
+  /**
+   *  Sign out the user upon button click.
+   */
+  function signOut(event) {
+    gapi.auth2.getAuthInstance().signOut();
   }
 </script>
 
 <h1>Config</h1>
 
-<br />API KEY:: {GOOGLE_DRIVE_API_KEY}
-<br />CLIENT ID: {GOOGLE_DRIVE_CLIENT_ID}
-<br />
+<h2>Sync with Google Drive</h2>
+{#if isSignedIn}
+  <button on:click={signOut}>Sign Out</button>
+{:else}
+  <button on:click={signIn}>Authorize</button>
+{/if}
+{#if error}
+  <strong style="color:red;">{error}</strong>
+{/if}
+
+<h2>Custom Services</h2>
 
 <svelte:head>
   <script
