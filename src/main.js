d9759719af32d26bc42386dc12658c5470b8d6ce
Chris Putnam <chris@webfx.com>
2021-08-25 07:17:01 -0400

    In progress - query parse before app load / mount

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import App from './App.svelte';
+import { config } from './store/config.js';
 
-var app = new App({
-  target: document.body
-});
+// Check for query string first, before loading App
+const query_pattern = /^\?q=([^&]+)(&|=|$)/;
+const query_match = document.location.search.match(query_pattern);
+
+if (query_match) {
+  alert(query_match);
+  console.log(config);
+} else {
+  var app = new App({
+    target: document.body
+  });
+}
 
 export default app;
