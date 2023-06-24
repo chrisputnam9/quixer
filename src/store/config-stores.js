6b6acdaf585080dd2e7cd674ba0357ea3176dbb7
Chris Putnam <chris@webfx.com>
2023-06-24 10:03:39 -0400

    Log out errors in addition to showing in interface

--- src/store/config-stores.js
+++ src/store/config-stores.js
@@ -8,11 +8,11 @@ export const configSyncIsSignedIn = writable(false);
  * Success state of current action:
  **/
 export const CONFIG_SYNC_SAVE_STATE = {
-  PENDING_LOGIN: 0,
-  PENDING: 1,
-  SAVING: 2,
-  SUCCESS: 3,
-  ERROR: 4
+	PENDING_LOGIN: 0,
+	PENDING: 1,
+	SAVING: 2,
+	SUCCESS: 3,
+	ERROR: 4
 };
 export const configSyncSaveState = writable(CONFIG_SYNC_SAVE_STATE.PENDING);
 
@@ -23,18 +23,23 @@ export const configSyncMessageShow = writable(false);
 export const configSyncMessageType = writable('');
 export const configSyncMessage = writable('');
 export const configSyncAlert = function (message, type = 'info') {
-  // Auto-set certain states
-  switch (type) {
-    case 'error':
-      configSyncSaveState.set(CONFIG_SYNC_SAVE_STATE.ERROR);
-      break;
-    case 'success':
-      configSyncSaveState.set(CONFIG_SYNC_SAVE_STATE.SUCCESS);
-      break;
-  }
-  configSyncMessageShow.set(true);
-  configSyncMessageType.set(type);
-  configSyncMessage.set(message);
+	// Auto-set certain states
+	switch (type) {
+		case 'error':
+			configSyncSaveState.set(CONFIG_SYNC_SAVE_STATE.ERROR);
+			console.error(message);
+			break;
+		case 'success':
+			configSyncSaveState.set(CONFIG_SYNC_SAVE_STATE.SUCCESS);
+			console.info(message);
+			break;
+		default:
+			console.log(message);
+			break;
+	}
+	configSyncMessageShow.set(true);
+	configSyncMessageType.set(type);
+	configSyncMessage.set(message);
 };
 
 /** Config Data **/
